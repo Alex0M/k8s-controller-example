acf9c3dd4450a4ed11db084acb534775ddbf7288
Alex0M <alex.sudom@gmail.com>
2025-06-20 22:40:51 -0500

    code refactoring

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"reflect"
 
 	api "github.com/Alex0M/k8s-controller-example/api/v1alpha1"
 	"github.com/Alex0M/k8s-controller-example/internal"
@@ -10,7 +11,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -42,71 +42,92 @@ func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 		frontendPageFound = false
 	}
 
-	var deployment appsv1.Deployment
-	var configmap corev1.ConfigMap
-	deployment.Name = req.Name
-	configmap.Name = req.Name
-
 	if frontendPageFound {
-		log.Info("checking deploymnet")
+		log.Info("reconciling configMap")
 
-		if err := r.Get(ctx, req.NamespacedName, &configmap); err != nil {
+		cm := internal.GetConfigMapObject(req.Name, req.Namespace, frontendPage.Spec.Contents)
+		if err := ctrl.SetControllerReference(&frontendPage, cm, r.Scheme()); err != nil {
+			return ctrl.Result{}, err
+		}
+
+		var existingConfigMap corev1.ConfigMap
+		existingConfigMap.Name = req.Name
+
+		if err := r.Get(ctx, req.NamespacedName, &existingConfigMap); err != nil {
 			if !apierrors.IsNotFound(err) {
-				log.Error(err, "unable to get configmap")
+				log.Error(err, "unable to get ConfigMap")
 				return ctrl.Result{}, err
 			}
-			log.Info("configmap not found")
-			if err := r.Create(ctx, internal.GetConfigMapObject(req.Name, req.Namespace, frontendPage.Spec.Contents)); err != nil {
-				log.Error(err, "unable to create configmap")
+
+			if err := r.Create(ctx, cm); err != nil {
 				return ctrl.Result{}, err
 			}
 		}
 
-		if err := r.Get(ctx, req.NamespacedName, &deployment); err != nil {
+		if !reflect.DeepEqual(existingConfigMap.Data, cm.Data) {
+			existingConfigMap.Data = cm.Data
+			if err := r.Update(ctx, &existingConfigMap); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+
+		//2. Ensure Deployment exists and up to date
+		log.Info("reconciling deployemnt")
+		dp := internal.GetDeploymentObject(req.Name, req.Namespace, frontendPage.Spec.Image, frontendPage.Spec.Replicas)
+		if err := ctrl.SetControllerReference(&frontendPage, dp, r.Scheme()); err != nil {
+			return ctrl.Result{}, err
+		}
+
+		var existingDeployemnt appsv1.Deployment
+		existingDeployemnt.Name = req.Name
+
+		if err := r.Get(ctx, req.NamespacedName, &existingDeployemnt); err != nil {
 			if !apierrors.IsNotFound(err) {
-				log.Error(err, "unable to get deployemnt")
+				log.Error(err, "unable to get Deployment")
 				return ctrl.Result{}, err
 			}
 
-			log.Info("deployemnt not found")
-			if err := r.Create(ctx, internal.GetDeploymentObject(req.Name, req.Namespace, frontendPage.Spec.Image, frontendPage.Spec.Replicas)); err != nil {
-				log.Error(err, "unable to create deployemnt")
-				return ctrl.Result{}, err
-			}
-		}
-
-		if err := r.Get(ctx, req.NamespacedName, &deployment); err != nil {
-			if !apierrors.IsNotFound(err) {
-				log.Error(err, "unable to get deployemnt")
+			if err := r.Create(ctx, dp); err != nil {
+				log.Error(err, "unable to create Deployment")
 				return ctrl.Result{}, err
 			}
 		}
 
-		if *deployment.Spec.Replicas != int32(frontendPage.Spec.Replicas) || deployment.Spec.Template.Spec.Containers[0].Image != frontendPage.Spec.Image {
-			deployment.Spec.Replicas = ptr.To[int32](int32(frontendPage.Spec.Replicas))
-			deployment.Spec.Template.Spec.Containers[0].Image = frontendPage.Spec.Image
-
-			if err := r.Update(ctx, &deployment); err != nil {
-				log.Error(err, "unable to update deployment")
-				return ctrl.Result{}, err
-			}
-
-			log.Info("deployemnt is updated")
-			return ctrl.Result{}, nil
+		dpUpdate := false
+		if *existingDeployemnt.Spec.Replicas != *dp.Spec.Replicas {
+			existingDeployemnt.Spec.Replicas = dp.Spec.Replicas
+			dpUpdate = true
 		}
 
-		log.Info("deployemnt is up to date")
+		if existingDeployemnt.Spec.Template.Spec.Containers[0].Image != dp.Spec.Template.Spec.Containers[0].Image {
+			existingDeployemnt.Spec.Template.Spec.Containers[0].Image = dp.Spec.Template.Spec.Containers[0].Image
+			dpUpdate = true
+		}
+
+		if dpUpdate {
+			if err := r.Update(ctx, &existingDeployemnt); err != nil {
+				if apierrors.IsConflict(err) {
+					return ctrl.Result{Requeue: true}, err
+				}
+				return ctrl.Result{}, err
+			}
+		}
+
 		return ctrl.Result{}, nil
 	} else {
 		log.Info("goging to delete config map")
+		var configmap corev1.ConfigMap
 		configmap.Namespace = req.Namespace
+		configmap.Name = req.Name
 		if err := r.Delete(ctx, &configmap); err != nil {
 			log.Error(err, "unable to delete configmap")
 			return ctrl.Result{}, err
 		}
 		log.Info("config map has been deleted")
 		log.Info("goging to delete config map")
+		var deployment appsv1.Deployment
 		deployment.Namespace = req.Namespace
+		deployment.Name = req.Name
 		if err := r.Delete(ctx, &deployment); err != nil {
 			log.Error(err, "unable to delete deployment")
 			return ctrl.Result{}, err
